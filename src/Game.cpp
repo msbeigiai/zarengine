cadf21a9aaf0532dfd03ae25d8299ce0f731184a
msbeigiai <msbeigi83@gmail.com>
2024-11-19 06:19:26 +0200

    Add framerate

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,29 +1,125 @@
 #include "Game.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
+
 Game::Game() {
+	isRunning = false;
 	std::cout << "Game constructor called\n";
 }
 
-Game::~Game(){
+Game::~Game() {
 	std::cout << "Game destructor called\n";
 }
 
-void Game::Initialize(){
+void Game::Initialize() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		std::cerr << "Error initializing SDL!" << std::endl;
+		return;
+	}
+	SDL_DisplayMode displayMode;
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	windowHeight = 800; // displayMode.h;
+	windowWidth = 600; // displayMode.w;
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		windowWidth,
+		windowHeight,
+		SDL_WINDOW_BORDERLESS
+	);
+	if (!window) {
+		std::cerr << "Error creating SDL window!" << std::endl;
+		return;
+	}
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	if (!renderer) {
+		std::cerr << "Error creating SDL renderer!" << std::endl;
+		return;
+	}
+
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
+	isRunning = true;
 }
 
-void Game::Run(){
-	while (true) {
+glm::vec2 playerPostion;
+glm::vec2 playerVelocity;
+
+void Game::Setup() {
+	playerPostion = glm::vec2(10.0, 20.0);
+	playerVelocity = glm::vec2(100.0, 0.0);
+}
+
+void Game::Update() {
+	int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
+	if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME) {
+		SDL_Delay(timeToWait);
+	}
+
+	double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+	
+	// Store the current frame time
+	millisecsPreviousFrame = SDL_GetTicks();
+
+	playerPostion.x += playerVelocity.x * deltaTime;
+	playerPostion.y += playerVelocity.y * deltaTime;
+}
+
+void Game::Run() {
+	Setup();
+	while (isRunning) {
 		ProcessInput();
 		Update();
 		Render();
 	}
 }
 
-void Game::ProcessInput(){}
+void Game::ProcessInput() {
+	SDL_Event sdlEvent;
+	while (SDL_PollEvent(&sdlEvent)) {
+		switch (sdlEvent.type) {
+		case SDL_QUIT:
+			isRunning = false;
+			break;
+		case SDL_KEYDOWN:
+			if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+				isRunning = false;
+			}
+			break;
+		}
+	}
+}
 
-void Game::Update(){}
+void Game::Render() {
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
+	SDL_RenderClear(renderer);
 
-void Game::Render(){}
+	// Draw png texture
+	SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
 
-void Game::Destroy(){}
+	SDL_Rect dstRect = {
+		static_cast<int>(playerPostion.x),
+		static_cast<int>(playerPostion.y),
+		32,
+		32
+	};
+
+	SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+
+	SDL_DestroyTexture(texture);
+
+	SDL_RenderPresent(renderer);
+}
+
+void Game::Destroy() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
