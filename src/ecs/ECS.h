df23cdc0a32d8d89739956d0cda377858037fb15
msbeigiai <msbeigi83@gmail.com>
2024-11-21 02:32:54 +0200

    Edit ECS

--- src/ecs/ECS.h
+++ src/ecs/ECS.h
@@ -1,6 +1,25 @@
 #pragma once
 
-class Component {};
+#include <bitset>
+#include <vector>
+
+const unsigned int MAX_COMPONENT = 32;
+
+// Signature
+typedef std::bitset<MAX_COMPONENT> Signature;
+
+struct IComponent {
+protected:
+	static int nextId;
+};
+
+template <typename T>
+class Component : public IComponent {
+	static const int GetId() {
+		static auto id = nextId++;
+		return id;
+	}
+};
 
 class Entity {
 private:
@@ -12,9 +31,28 @@ public:
 };
 
 class System {
-	
+private:
+	Signature componentSignature;
+	std::vector<Entity> entities;
+
+public:
+	System() = default;
+	virtual ~System() = default;
+
+	void AddEntityToSystem(Entity entity);
+	void RemoveEntityFromSystem(Entity entity);
+	std::vector<Entity> GetSystemEntities() const;
+	const Signature& GetComponentSignature() const;
+
+	template <typename TComponent> void RequireComponent();
 };
 
 class Registry {};
+
+template <typename TComponent>
+void System::RequireComponent() {
+	const auto componentId = Component<TComponent>::GetId();
+	componentSignature.set(componentId);
+}
 
 
