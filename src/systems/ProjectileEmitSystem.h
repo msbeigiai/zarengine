ef4499c55cd34e49abc666f46315b5d8dcd8e5e0
msbeigiai <msbeigi83@gmail.com>
2024-12-18 07:58:40 +0200

    Rename file

---
+++ src/systems/ProjectileEmitSystem.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../ecs/ECS.h"
+#include "../components/RigidBodyComponent.h"
+#include "../components/SpriteComponent.h"
+#include "../components/BoxColliderComponent.h"
+#include "../components/ProjectileEmitterComponent.h"
+#include <SDL.h>
+
+
+
